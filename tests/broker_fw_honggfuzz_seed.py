83680440b11e882a1e19b1b0b11ff823c38ce9a5
RobinDavid <dev.robin.david@gmail.com>
2020-08-14 12:01:37 +0200

    add script to forward all files of a directory with inotify

--- tests/broker_fw_honggfuzz_seed.py
+++ tests/broker_fw_honggfuzz_seed.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 import logging
 from typing import Tuple
+import sys
+from pathlib import Path
 
+import inotify.adapters
 
 from libpastis.agent import BrokerAgent
 from libpastis.types import SeedType, FuzzingEngine, LogLevel, Arch, State
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
 
+clients = set()
+
 
 def seed_received(cli_id: bytes, typ: SeedType, seed: bytes, origin: FuzzingEngine):
-    global agent
     logging.info(f"[{cli_id.hex()}] [SEED] [{origin.name}] {seed.hex()} ({typ.name})")
-    agent.send_seed(cli_id, typ, seed[::-1], origin)
 
 
 def hello_received(cli_id: bytes, engines: Tuple[FuzzingEngine, str], arch: Arch, cpus: int, memory: int):
+    global clients
+    if cli_id not in clients:
+        logging.info(f"[broker] new client: {cli_id.hex()}")
+        clients.add(cli_id)
     logging.info(f"[{cli_id.hex()}] [HELLO] Arch:{arch.name} engines:{[x[0].name for x in engines]} (cpu:{cpus}, mem:{memory})")
 
 
@@ -24,9 +31,6 @@ def log_received(cli_id: bytes, level: LogLevel, message: str):
 
 
 def telemetry_received(cli_id: bytes, *args):
-    # state: State = None, exec_per_sec: int = None, total_exec: int = None,
-    # cycle: int = None, timeout: int = None, coverage_block: int = None, coverage_edge: int = None,
-    # coverage_path: int = None, last_cov_update: int = None):
     logging.info(f"[{cli_id.hex()}] [TELEMETRY] [{args}")
 
 
@@ -45,4 +49,27 @@ if __name__ == "__main__":
     agent.register_telemetry_callback(telemetry_received)
     agent.register_stop_coverage_callback(stop_coverage_received)
 
-    agent.run()
+    agent.start()
+
+    # Now start listening on a seed folder given in parameter
+    # and send them to all clients that have at least sent
+    # once a message
+
+    i = inotify.adapters.Inotify()
+
+    i.add_watch(sys.argv[1])
+
+    for event in i.event_gen():
+        if event is not None:
+            (header, type_names, watch_path, filename) = event
+
+            if 'IN_CLOSE_WRITE' in type_names:
+                file = Path(watch_path) / filename
+                bts = file.read_bytes()
+                print(f"send: {filename}")
+                for cli in clients:
+                    agent.send_seed(cli, SeedType.INPUT, bts, FuzzingEngine.HONGGFUZZ)
+
+'''
+PYTHONPATH=. python3  ./tests/broker_fw_honggfuzz_seed.py /tmp/toto 
+'''
