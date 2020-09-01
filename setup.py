a94c16753e2e904451c5c4db7032c32321a52508
Christian Heitman <cheitman@quarkslab.com>
2020-09-01 16:33:48 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hfwrapper",
+    version="0.1",
+    description="Honggfuzz wrapper",
+    packages=find_packages(),
+    setup_requires=[],
+    install_requires=[]
+)
