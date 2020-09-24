fcd1f469bccbe5cab328b8df614a621e7c2c35b7
Robin David <rdavid@quarkslab.com>
2020-09-24 18:21:43 +0200

    WIP: first version of pastis broker

--- pastisbroker/broker.py
+++ pastisbroker/broker.py
@@ -1,17 +1,27 @@
 # built-in imports
 import logging
-from typing import Tuple, Generator
+from typing import Tuple, Generator, List
 from pathlib import Path
-from time import gmtime, strftime
+import time
 from hashlib import md5
+from enum import Enum
+from collections import Counter
 
 # Third-party imports
-from libpastis import BrokerAgent
-from libpastis.types import SeedType, FuzzingEngine, LogLevel, Arch
+from libpastis import BrokerAgent, do_engine_support_coverage_strategy
+from libpastis.types import SeedType, FuzzingEngine, LogLevel, Arch, State, SeedInjectLoc, CheckMode, CoverageMode, ExecMode
 from klocwork import KlocworkReport
+import lief
 
 # Local imports
 from .client import PastisClient
+from .stat_manager import StatManager
+
+
+class BrokingMode(Enum):
+    FULL = 1              # Transmit all seeds to all peers
+    NO_TRANSMIT = 2       # Does not transmit seed to peers (for comparing perfs of tools against each other)
+    COVERAGE_ORDERED = 3  # Transmit a seed to a peer if they have the same notion of coverage
 
 
 class PastisBroker(BrokerAgent):
@@ -23,14 +33,21 @@ class PastisBroker(BrokerAgent):
 
     KL_MAGIC = "KL-METADATA"
 
-    def __init__(self, workspace, kl_report, binaries_dir):
+    def __init__(self, workspace, kl_report, binaries_dir, broker_mode: BrokingMode, check_mode: CheckMode = CheckMode.CHECK_ALL, p_argv: List[str] = []):
         super(PastisBroker, self).__init__()
         self.workspace = Path(workspace)
         self._init_workspace()
 
+        # Init internal state
+        self.mode = broker_mode
+        self.ck_mode = check_mode
+        self.inject = SeedInjectLoc.STDIN  # At the moment injection location is hardcoded
+        self.argv = p_argv
+        self.engines_args = {x: "" for x in FuzzingEngine}
+
         # Initialize availables binaries
         self.programs = {}  # Tuple[(Arch, Fuzzer, ExecMode)] -> Path
-        self._find_binary_workspace()
+        self._find_binary_workspace(binaries_dir)
 
         # Klocwork informations
         self.kl_report = KlocworkReport(kl_report)
@@ -44,6 +61,11 @@ class PastisBroker(BrokerAgent):
         # Runtime infos
         self._running = False
         self._seed_pool = {}  # Seed bytes -> (SeedType, origin)
+        self._start_time = None
+        self._stop = False
+
+        # Create the stat manager
+        self.statmanager = StatManager()
 
     @property
     def running(self) -> bool:
@@ -79,22 +101,26 @@ class PastisBroker(BrokerAgent):
 
     def seed_received(self, cli_id: bytes, typ: SeedType, seed: bytes, origin: FuzzingEngine):
         cli = self.clients[cli_id]
+        is_new = seed not in self._seed_pool
+        self.statmanager.update_seed_stat(cli, typ, is_new)
 
         # Show log message and save seed to file
-        if seed not in self._seed_pool:
+        if is_new:
             logging.info(f"[{cli.strid}] [SEED] [{origin.name}] {seed.hex()} ({typ.name})")
             self.write_seed(typ, cli, seed) # Write seed to file
             self._seed_pool[seed] = (typ, origin)  # Save it in the local pool
 
         # Iterate on all clients and send it to whomever never received it
-        for c in self.iter_other_clients(cli):
-            if c.is_new_seed(seed):
-                self.send_seed(c.netid, typ, seed, origin)  # send the seed to the client
-                c.add_seed(seed)  # Add it in its list of seed
+        if self.mode == BrokingMode.FULL:
+            for c in self.iter_other_clients(cli):
+                if c.is_new_seed(seed):
+                    self.send_seed(c.netid, typ, seed, origin)  # send the seed to the client
+                    c.add_seed(seed)  # Add it in its list of seed
+        # TODO: implementing BrokingMode.COVERAGE_ORDERED
 
     def write_seed(self, typ: SeedType, from_cli: PastisClient, seed: bytes):
-        time = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
-        fname = f"{time}_{from_cli.strid}_{md5(seed).hexdigest()}.cov"
+        t = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+        fname = f"{t}_{from_cli.strid}_{md5(seed).hexdigest()}.cov"
         p = self.workspace / self._seed_typ_to_dir(typ) / fname
         p.write_bytes(seed)
 
@@ -104,41 +130,166 @@ class PastisBroker(BrokerAgent):
         logging.info(f"[{client.strid}] [HELLO] Arch:{arch.name} engines:{[x[0].name for x in engines]} (cpu:{cpus}, mem:{memory})")
         self.clients[client.netid] = client
 
-        # A client is coming in the middle of a session
-        if self.running:
-            # TODO: Send him the start message immediately (balancing engines etc..)
-            # TODO: Send him all seeds that have already transited here
-            pass
+        if self.running: # A client is coming in the middle of a session
+            self.start_client(client)
+            # Iterate all the seed pool and send it to the client
+            if self.mode == BrokingMode.FULL:
+                for seed, (typ, orig) in self._seed_pool.items():
+                    self.send_seed(client.netid, typ, seed, orig)  # necessarily a new seed
+                    client.add_seed(seed)  # Add it in its list of seed
+                # TODO: Implementing BrokingMode.COVERAGE_ORDERED
 
     def log_received(self, cli_id: bytes, level: LogLevel, message: str):
-        logging.info(f"[{cli_id.hex()}] [LOG] [{level.name}] {message}")
-        # TODO: To implement
-        # TODO: Implementer le *magic* pour recevoir les infos de defaut, vulns
+        client = self.clients[cli_id]
+        logging.info(f"[{client.strid}] [LOG] [{level.name}] {message}")
+        client.log(level, message)
+        if message.startswith(self.KL_MAGIC):
+            pass # TODO: Retrieving info about the defaut, vulns (covered / validated)
 
-    def telemetry_received(self, cli_id: bytes, *args):
-        # state: State = None, exec_per_sec: int = None, total_exec: int = None,
-        # cycle: int = None, timeout: int = None, coverage_block: int = None, coverage_edge: int = None,
-        # coverage_path: int = None, last_cov_update: int = None):
-        logging.info(f"[{cli_id.hex()}] [TELEMETRY] [{args}")
+    def telemetry_received(self, cli_id: bytes,
+        state: State = None, exec_per_sec: int = None, total_exec: int = None,
+        cycle: int = None, timeout: int = None, coverage_block: int = None, coverage_edge: int = None,
+        coverage_path: int = None, last_cov_update: int = None):
+        client = self.clients[cli_id]
+        # NOTE: ignore state (shall we do something of it?)
+        args = [('-' if not x else x) for x in [exec_per_sec, total_exec, cycle, timeout, coverage_block, coverage_edge, coverage_path, last_cov_update]]
+        client.log(LogLevel.INFO, "exec/s:{} tot_exec:{} cycle:{} To:{} CovB:{} CovE:{} CovP:{} last_up:{}".format(args))
+
+        # Update all stats in the stat manager (for later UI update)
+        self.statmanager.set_exec_per_sec(client, exec_per_sec)
+        self.statmanager.set_total_exec(client, total_exec)
+        self.statmanager.set_cycle(client, cycle)
+        self.statmanager.set_timeout(client, timeout)
+        self.statmanager.set_coverage_block(client, coverage_block)
+        self.statmanager.set_coverage_edge(client, coverage_edge)
+        self.statmanager.set_coverage_path(client, coverage_path)
+        self.statmanager.set_las_coverage_update(client, last_cov_update)
+        # NOTE: Send an update signal for future UI ?
 
     def stop_coverage_received(self, cli_id: bytes):
-        logging.info(f"[{cli_id.hex()}] [STOP_COVERAGE]")
-        # TODO: To implement
+        client = self.clients[cli_id]
+        logging.info(f"[{client.strid}] [STOP_COVERAGE]")
+        for c in self.iter_other_clients(client):
+            c.set_stopped()
+            self.send_stop(c.netid)
 
     def start_client(self, client: PastisClient):
-        # TODO: Find the appropriate binary for the client
-        # Send him the start message
-        pass
+        program = None  # The program yet to be selected
+        engine = None
+        exmode = ExecMode.SINGLE_EXEC
+        engines = Counter({e: 0 for e in FuzzingEngine})
+        engines.update(c.engine for c in self.clients.values() if c.is_running())  # Count instances of each engine running
+        for eng, _ in engines.most_common()[::-1]:
+            # If the engine is not supported by the client continue
+            if not client.is_supported_engine(eng):
+                continue
+
+            # Try finding a suitable binary for the current engine and the client arch
+            program = self.programs.get((client.arch, eng, ExecMode.PERSISTENT))
+            if program:
+                exmode = ExecMode.PERSISTENT  # a program was found in persistent mode
+            else:
+                program = self.programs.get((client.arch, eng, ExecMode.SINGLE_EXEC))
+            if not program:  # If still no program was found continue iterating engines
+                continue
+
+            # Valid engine and suitable program found
+            engine = eng
+
+            # Now that program have been found select coverage strategy
+            if do_engine_support_coverage_strategy(engine):
+                covs = Counter({c: 0 for c in CoverageMode})
+                # Count how many times each coverage strategies have been launched
+                covs.update(x.coverage_mode for x in self.clients.values() if x.is_running() and x.engine == engine)
+                # Revert dictionnary to have freq -> [covmodes]
+                d = {v: [] for v in covs.values()}
+                for cov, count in covs.items():
+                    d[count].append(cov)
+                # pick in-order BLOCK < EDGE < PATH among the least frequently launched modes
+                covmode = sorted(d[min(d)])[0]
+            else:
+                covmode = None
+
+            # If got here a suitable program has been found just break loop
+            break
+
+        if engine is None or program is None:
+            logging.critical(f"No suitable engine or program was found for client {client.strid} {client.engines}")
+            return
+
+        # Update internal client info and send him the message
+        client.set_running(engine, covmode, exmode, self.ck_mode)
+        self.send_start(client.netid,
+                        program,
+                        self.argv,
+                        exmode,
+                        self.ck_mode,
+                        covmode,
+                        engine,
+                        self.engines_args[engine],
+                        self.inject,
+                        self.kl_report.to_json())
 
     def run(self):
-        # TOOD: Set the start timer
-        # TODO: Send the start message to all clients already connected
-        pass
+        self._start_time = time.localtime()
 
-    def _find_binary_workspace(self):
-        # TODO: iterate all files parse them with lief and
-        # TODO: fill programs
-        pass
+        # Send the start message to all clients
+        for c in self.clients.values():
+            if not c.is_running():
+                self.start_client(c)
+
+        # Start infinite loop
+        while True:
+            time.sleep(0.1)
+            if self._stop:
+                logging.info("broker terminate")
+                break
+
+    def _find_binary_workspace(self, binaries_dir) -> None:
+        """
+        Iterate the whole directory to find suitables binaries in the
+        various architecture, and compiled for the various engines.
+
+        :param binaries_dir: directory containing the various binaries
+        :return: None
+        """
+        d = Path(binaries_dir)
+        for file in d.iterdir():
+            if file.is_file():
+                p = lief.parse(str(file))
+                if not p:
+                    continue
+                if not isinstance(p, lief.ELF.Binary):
+                    logging.warning(f"binary {file} not supported (only ELF at the moment)")
+                    continue
+
+                good = False
+                honggfuzz = False
+                exmode = ExecMode.SINGLE_EXEC
+                for f in p.functions:
+                    name = f.name
+                    if '__klocwork' in name:
+                        good = True
+                    if '__sanitizer' in name:
+                        honggfuzz = True
+                if not good:
+                    logging.info(f"ignore binary: {file} (does not contain klocwork intrinsics)")
+                    continue
+                if 'HF_ITER' in (x.name for x in p.imported_functions):
+                    exmode = ExecMode.PERSISTENT
+                mapping = {lief.ELF.ARCH.x86_64: Arch.X86_64,
+                           lief.ELF.ARCH.i386: Arch.X86,
+                           lief.ELF.ARCH.ARM: Arch.ARMV7,
+                           lief.ELF.ARCH.AARCH64: Arch.AARCH64}
+                arch = mapping.get(p.header.machine_type)
+                if arch:
+                    engine = FuzzingEngine.HONGGFUZZ if honggfuzz else FuzzingEngine.TRITON
+                    tup = (arch, engine, exmode)
+                    if tup in self.programs:
+                        logging.warning(f"binary with same properties {tup} already detected, drop: {file}")
+                    else:
+                        self.programs[tup] = file
+                        logging.info(f"new binary detected [{arch}, {engine}, {exmode}]: {file}")
 
     def _init_workspace(self):
         """ Create the directory for inputs, crashes and logs"""
@@ -151,3 +302,8 @@ class PastisBroker(BrokerAgent):
         return {SeedType.INPUT: self.INPUT_DIR,
                 SeedType.CRASH: self.CRASH_DIR,
                 SeedType.HANG: self.HANGS_DIR}[typ]
+
+    def set_engine_args(self, engine: FuzzingEngine, args: str):
+        if self.engines_args[engine]:
+            logging.warning(f"arguments where already set for engine {engine.name}")
+        self.engines_args[engine] = args
---
+++ pastisbroker/stat_manager.py
@@ -0,0 +1,70 @@
+# Built-in imports
+import time
+
+# Third-party importq
+from libpastis.types import SeedType
+
+# Local imports
+from pastisbroker.client import PastisClient
+
+
+class StatManager(object):
+    """
+    Keeps temporal statistics to plot them.
+    """
+    def __init__(self):
+        self.cli_coverage = {}  # Client -> List[]
+        self.cli_inputs = {}    # Client -> Tuple[new_seed, total_seed]
+        self.cli_crashs = {}
+        self.cli_hangs = {}
+
+    def update_seed_stat(self, client: PastisClient, typ: SeedType, new: bool) -> None:
+        t = time.localtime()
+        info = {SeedType.INPUT: self.cli_inputs, SeedType.CRASH: self.cli_crashs, SeedType.HANG: self.cli_hangs}[typ]
+        if client not in info:
+            info[client.netid] = ([], [])
+        data = info[client.netid]
+        # Update total submitted seeds and update stats
+        client.seed_submitted_count += 1
+        data[1].append((t, client.seed_submitted_count))
+
+        if new:
+            client.seed_first += 1
+            data[0].append((t, client.seed_first))
+
+    def set_exec_per_sec(self, client: PastisClient, exec_per_sec: int = None):
+        if exec_per_sec:
+            client.exec_per_sec = exec_per_sec  # instantaneous value does not keep history
+
+    def set_total_exec(self, client: PastisClient, total_exec: int = None):
+        if total_exec:
+            client.total_exec = total_exec  # instantaneous value does not keep history
+
+    def set_cycle(self, client: PastisClient, cycle: int = None):
+        if cycle:
+            client.cycle = cycle  # instantaneous value does not keep history
+
+    def set_timeout(self, client: PastisClient, timeout: int = None):
+        if timeout:
+            client.timeout = timeout  # instantaneous value does not keep history
+
+    def _set_coverage(self, client: PastisClient, coverage: int = None):
+        t = time.localtime()
+        client.coverage_block = coverage
+        if client.netid not in self.cli_coverage:
+            self.cli_coverage[client.netid] = []
+        self.cli_coverage[client.netid].append((t, coverage))
+
+    def set_coverage_block(self, client: PastisClient, coverage: int = None):
+        self._set_coverage(client, coverage)
+
+    def set_coverage_edge(self, client: PastisClient, coverage: int = None):
+        self._set_coverage(client, coverage)
+
+    def set_coverage_path(self, client: PastisClient, coverage: int = None):
+        self._set_coverage(client, coverage)
+
+    def set_las_coverage_update(self, client: PastisClient, last_up: int = None):
+        if last_up:
+            client.last_cov_update = last_up  # instantaneous value does not keep history
+
