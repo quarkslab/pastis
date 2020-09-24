7ea520a7938a916ce0774d188747caf64c083a69
RobinDavid <dev.robin.david@gmail.com>
2020-09-24 23:19:25 +0200

    add function to get local architecture

--- libpastis/utils.py
+++ libpastis/utils.py
@@ -1,4 +1,11 @@
-from .types import FuzzingEngine
+# builtin imports
+from typing import Optional
+
+# Third-party import
+import psutil
+
+# Local imports
+from .types import FuzzingEngine, Arch
 
 
 def do_engine_support_coverage_strategy(engine: FuzzingEngine) -> bool:
@@ -13,3 +20,10 @@ def do_engine_support_coverage_strategy(engine: FuzzingEngine) -> bool:
     """
     return {FuzzingEngine.TRITON: True,
              FuzzingEngine.HONGGFUZZ: False}[engine]
+
+
+def get_local_architecture() -> Optional[Arch]:
+    mapping = {"i386": Arch.X86, "x86_64": Arch.X86_64, "armv7l": Arch.ARMV7, "aarch64": Arch.AARCH64}
+    a = psutil.os.uname().machine
+    # FIXME: Make sure psutil.os.uname().machine returns this string as architecture
+    return mapping.get(a)
