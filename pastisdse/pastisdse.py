2bedcab6ac571f1b86fb09fea10cf90bd7e72c73
Jonathan Salwan <jonathan.salwan@gmail.com>
2020-11-02 19:58:54 +0100

    Add a debug function

--- pastisdse/pastisdse.py
+++ pastisdse/pastisdse.py
@@ -97,6 +97,7 @@ class PastisDSE(object):
         #self.dse.callback_manager.register_new_input_callback(self.checksum_callback)   # must be the first cb
         self.dse.callback_manager.register_new_input_callback(self.send_seed_to_broker) # must be the second cb
         self.dse.callback_manager.register_function_callback('__klocwork_alert_placeholder', self.intrinsic_callback)
+        #self.dse.callback_manager.register_post_instuction_callback(self.trace_debug)
 
         #if chkmode == CheckMode.CHECK_ALL:
         #    self.dse.callback_manager.register_probe_callback(UAFSanitizer())
@@ -106,6 +107,9 @@ class PastisDSE(object):
         #    # TODO Buffer overflow
         #elif chkmode == CheckMode.ALERT_ONLY:
         #    self.dse.callback_manager.register_function_callback('__klocwork_alert_placeholder', self.intrinsic_callback)
+
+    def trace_debug(self, se: SymbolicExecutor, state: ProcessState, instruction: 'Instruction'):
+        print("[tid:%d] %#x: %s" %(instruction.getThreadId(), instruction.getAddress(), instruction.getDisassembly()))
 
 
     def seed_received(self, typ: SeedType, seed: bytes, origin: FuzzingEngine):
