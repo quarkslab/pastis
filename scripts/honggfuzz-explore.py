446cb1d77b6ac3760719508b19259cd839b08f7e
Robin David <rdavid@quarkslab.com>
2020-11-15 19:44:21 +0100

    multiple improvements +bugfix

--- scripts/honggfuzz-explore.py
+++ scripts/honggfuzz-explore.py
@@ -8,7 +8,7 @@ from hfwrapper import Honggfuzz
 from libpastis.agent import FileAgent
 
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
 
 def init_arg_parser():
@@ -32,10 +32,12 @@ def main(args):
     try:
         logging.info(f'Starting fuzzer...')
 
-        logging.debug(f'Target: {args.target}')
-        logging.debug(f'Target arguments: {args.target_arguments}')
+        logging.info(f'Target: {args.target}')
+        logging.info(f'Target arguments: {args.target_arguments}')
 
-        hfuzz.run(target=args.target, target_arguments=args.target_arguments)
+        arguments = '' if args.target_arguments is None else args.target_arguments
+
+        hfuzz.run(target=args.target, target_arguments=arguments)
     except KeyboardInterrupt:
         print("[!] CTRL+C detected! Aborting...")
 
