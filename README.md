2343b162c20fd6cfab6405334b673e817e7e01f4
RobinDavid <dev.robin.david@gmail.com>
2020-11-25 23:32:24 +0100

    add base pastisd

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # pastisd
 
-Pastis daemon that will be running on the system
+Pastis daemon that will be running on the embedded device.
+
+Its main task is to connect to the broker advertisizing all the engines installed locally.
+The broker then decide which engine to start, and the pastisd will start the appropriate one.
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""Installation script for the libpastis module."""
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name="pastisd",
+    version="0.1",
+    description="PASTIS deamon initializing connection with broker",
+    packages=find_packages(),
+    setup_requires=[],
+    install_requires=[],
+    tests_require=[],
+    license="qb",
+    author="Quarkslab",
+    classifiers=[
+        'Topic :: Security',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+    ],
+    test_suite="",
+    scripts=['bin/pastisd']
+)
