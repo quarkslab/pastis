43596fd230b737ce94c37ee27d40d455e8387420
RobinDavid <dev.robin.david@gmail.com>
2021-01-25 15:55:44 +0100

    version 0.2

--- pastisdse/__init__.py
+++ pastisdse/__init__.py
@@ -2,4 +2,7 @@ from .pastisdse import PastisDSE
 
 # Expose triton version
 import tritondse
-__version__ = tritondse.TRITON_VERSION
+
+__version__ = "0.2"
+
+TRITON_VERSION = tritondse.TRITON_VERSION
