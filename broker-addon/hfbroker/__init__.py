18f03afa5f104d9702a795bc96ecdefdabbfaabf
RobinDavid <dev.robin.david@gmail.com>
2022-01-27 19:07:53 +0100

    fix addon

--- broker-addon/hfbroker/__init__.py
+++ broker-addon/hfbroker/__init__.py
@@ -4,10 +4,36 @@ from typing import Union, Tuple, List, Optional
 
 # third-party import
 import lief
+from typing import Type
 
 from libpastis import FuzzingEngineDescriptor, EngineConfiguration
-from libpastis.types import ExecMode, CoverageMode
+from libpastis.types import ExecMode, CoverageMode, FuzzMode
 
+
+class HonggfuzzConfiguration(EngineConfiguration):
+    """
+    Small wrapping function for AFL++ additional parameters
+    """
+    def __init__(self, data: str):
+        """
+        :param data: command line to provide AFL++ as-is
+        """
+        self.data = data
+
+    @staticmethod
+    def from_file(filepath: Path) -> 'HonggfuzzConfiguration':
+        return HonggfuzzConfiguration(Path(filepath).read_text())
+
+    @staticmethod
+    def from_str(s: str) -> 'HonggfuzzConfiguration':
+        return HonggfuzzConfiguration(s)
+
+    def to_str(self) -> str:
+        return self.data
+
+    def get_coverage_mode(self) -> CoverageMode:
+        """ Current coverage mode selected in the file """
+        raise CoverageMode.AUTO
 
 
 class HonggfuzzEngineDescriptor(FuzzingEngineDescriptor):
@@ -22,10 +48,10 @@ class HonggfuzzEngineDescriptor(FuzzingEngineDescriptor):
         pass
 
     @staticmethod
-    def accept_file(binary_file: Path) -> Tuple[bool, Optional[ExecMode]]:
+    def accept_file(binary_file: Path) -> Tuple[bool, Optional[ExecMode], Optional[FuzzMode]]:
         p = lief.parse(str(binary_file))
         if not p:
-            return False, None
+            return False, None, None
 
         # Search for HF instrumentation
         good = False
@@ -34,7 +60,7 @@ class HonggfuzzEngineDescriptor(FuzzingEngineDescriptor):
                 good = True
                 break
         if not good:
-            return False, None
+            return False, None, None
 
         # Search for persistent magic
         exmode = ExecMode.SINGLE_EXEC  # by default single_exec
@@ -46,12 +72,12 @@ class HonggfuzzEngineDescriptor(FuzzingEngineDescriptor):
         else:
             if 'HF_ITER' in (x.name for x in p.imported_functions):  # More dummy method
                 exmode = ExecMode.PERSISTENT
-        return True, exmode
+        return True, exmode, FuzzMode.INSTRUMENTED  # FIXME: Can honggfuzz work in binary_only?
 
     @staticmethod
     def supported_coverage_strategies() -> List[CoverageMode]:
         return [CoverageMode.AUTO]
 
     @staticmethod
-    def configuration() -> EngineConfiguration:
-        raise NotImplementedError()
+    def get_configuration_cls() -> Type[EngineConfiguration]:
+        return HonggfuzzConfiguration
