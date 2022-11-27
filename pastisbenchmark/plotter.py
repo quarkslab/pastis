ff3121a4fcd74dc53cea3ee849a697a8c29b1527
Robin David <rdavid@quarkslab.com>
2022-11-27 12:41:17 +0100

    update benchmark code

--- pastisbenchmark/plotter.py
+++ pastisbenchmark/plotter.py
@@ -17,14 +17,16 @@ class Plotter(object):
     FONT_SZ = 15
     LEGEND_SZ = 14
 
+    SEED_FUZZER = "seeds"
+    ALL_FUZZER = "all"
     PLOT_DIR = "plots"
 
     def __init__(self, name: str):
         self.fig, (self.ax1, self.ax2) = plt.subplots(1, 2)
         self.name = name
 
-        self._configure_plot(self.ax1, ylabel="coverage (edge)")
-        self._configure_plot(self.ax2, ylabel="coverage (edge)")
+        # self._configure_plot(self.ax1, ylabel="coverage (edge)")
+        # self._configure_plot(self.ax2, ylabel="coverage (edge)", is_log=True)
 
     def _configure_plot(self, plot, ylabel: str, is_log: bool = False):
         plot.tick_params(axis='both', which='major', labelsize=self.TICK_SZ)
@@ -40,15 +42,16 @@ class Plotter(object):
     def add_campaign_to_plot(self, campaign: CampaignResult):
         """ Iterate all stat_items and generate coverage plot."""
         for fuzzer, results in campaign.results:
+            if fuzzer == self.SEED_FUZZER:
+                continue
             self.add_to_plot(self.ax1, fuzzer, results)
             self.add_to_plot(self.ax2, fuzzer, results)
 
     def add_to_plot(self, plot, fuzzer: str, results: List[InputCovDelta], annotate_tt=False, label_tt=False):
-        X = [x.time_elapsed for x in  results]
-        Y = [x.total_coverage for x in  results]
+        X = [x.time_elapsed for x in results]
+        Y = [x.total_coverage for x in results]
 
-        F = [x.fuzzer for x in  results]
-
+        F = [x.fuzzer for x in results]
         plot.plot(X, Y, label=fuzzer, linewidth=2)
 
         # if annotate_tt:
@@ -70,6 +73,8 @@ class Plotter(object):
     #     return t, y
 
     def show(self):
+        self._configure_plot(self.ax1, ylabel="coverage (edge)")
+        self._configure_plot(self.ax2, ylabel="coverage (edge)", is_log=True)
         plt.show()
 
     def save_to(self, dir: Union[str, Path]) -> None:
