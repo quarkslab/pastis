9ce7d9dbaa8a71b7e5f672a8b860b6c83546fd60
RobinDavid <dev.robin.david@gmail.com>
2022-11-27 23:44:50 +0100

    update benchmark scripts

--- pastisbenchmark/plotter.py
+++ pastisbenchmark/plotter.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # local imports
 from pastisbenchmark.replayer import ReplayType
 from pastisbenchmark.results import InputCovDelta, CampaignResult
-
+from tritondse import Config, CoverageStrategy, SmtSolver, BranchSolvingStrategy
 
 
 class Plotter(object):
@@ -45,8 +45,8 @@ class Plotter(object):
         for fuzzer, results in campaign.results:
             if fuzzer == self.SEED_FUZZER:
                 continue
-            self.add_to_plot(self.ax1, fuzzer, results)
-            self.add_to_plot(self.ax2, fuzzer, results)
+            self.add_to_plot(self.ax1, self.format_fuzzer_name(campaign, fuzzer), results)
+            self.add_to_plot(self.ax2, self.format_fuzzer_name(campaign, fuzzer), results)
 
     def add_to_plot(self, plot, fuzzer: str, results: List[InputCovDelta], annotate_tt=False, label_tt=False):
         X = [x.time_elapsed for x in results]
@@ -54,6 +54,27 @@ class Plotter(object):
 
         F = [x.fuzzer for x in results]
         plot.plot(X, Y, label=fuzzer, linewidth=2)
+
+    def format_fuzzer_name(self, campaign: CampaignResult, fuzzer: str) -> str:
+        if fuzzer == self.ALL_FUZZER:
+            return campaign.slug_name
+        elif "TT" in fuzzer:
+            config = campaign.fuzzers_config[fuzzer]
+            cov_name = {CoverageStrategy.BLOCK: "B", CoverageStrategy.EDGE: "E", CoverageStrategy.PREFIXED_EDGE:"PE",
+                        CoverageStrategy.PATH:"P"}[config.coverage_strategy]
+            param = [
+                "R" if BranchSolvingStrategy.COVER_SYM_READ in config else "-",
+                "W" if BranchSolvingStrategy.COVER_SYM_WRITE in config else "-",
+                "J" if BranchSolvingStrategy.COVER_SYM_DYNJUMP in config else "-"
+            ]
+            solver = {SmtSolver.Z3: "Z3", SmtSolver.BITWUZLA: "BZLA"}[config.smt_solver]
+            return f"TritonDSE[{cov_name}][{''.join(param)}][{solver}]"
+        elif "AFLPP" in fuzzer:
+            return "AFL++"
+        elif "HF" in fuzzer:
+            return "Honggfuzz"
+        else:
+            assert False
 
     def add_triton_input(self, campaign: CampaignResult):
         if campaign.is_full_duplex:
@@ -71,5 +92,51 @@ class Plotter(object):
         plt.show()
 
     def save_to(self, dir: Union[str, Path]) -> None:
-        # TODO: to implement !
-        pass
+        plt.savefig(dir / "plot.pdf", dpi=600)
+
+    def print_input_number_stats(self, campaign: CampaignResult):
+        tot_input = sum(len(y) for x, y in campaign.results)
+
+        stats = {}  # Fuzzer -> count
+
+        for fuzzer, results in campaign.results:
+            for item in results:
+                if fuzzer == campaign.ALL_FUZZER:
+                    date, elapsed, fuzzer_id, hash = campaign.parse_filename(item.input_name)
+                    if fuzzer_id in stats:
+                        stats[fuzzer_id] += 1
+                    else:
+                        stats[fuzzer_id] = 1
+                else:
+                    if fuzzer in stats:
+                        stats[fuzzer] += 1
+                    else:
+                        stats[fuzzer] = 1
+        print("Input number stats:")
+        for fuzzer, n in stats.items():
+            print(f"- {fuzzer}: {n}  [{n / tot_input:.2%}]")
+        print(f"Total: {tot_input}")
+
+
+    def print_coverage_stats(self, campaign: CampaignResult):
+        tot_input = sum(len(z.unique_coverage) for x, y in campaign.results for z in y)
+
+        stats = {}  # Fuzzer -> count
+
+        for fuzzer, results in campaign.results:
+            for item in results:
+                if fuzzer == campaign.ALL_FUZZER:
+                    date, elapsed, fuzzer_id, hash = campaign.parse_filename(item.input_name)
+                    if fuzzer_id in stats:
+                        stats[fuzzer_id] += len(item.unique_coverage)
+                    else:
+                        stats[fuzzer_id] = len(item.unique_coverage)
+                else:
+                    if fuzzer in stats:
+                        stats[fuzzer] += len(item.unique_coverage)
+                    else:
+                        stats[fuzzer] = len(item.unique_coverage)
+        print("Coverage stats:")
+        for fuzzer, n in stats.items():
+            print(f"- {fuzzer}: {n}  [{n / tot_input:.2%}]")
+        print(f"Total: {tot_input}")
