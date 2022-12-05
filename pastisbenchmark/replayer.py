db05883954b2828b7d66ccddb973f809f8538d1c
RobinDavid <dev.robin.david@gmail.com>
2022-12-05 15:53:16 +0100

    add timeout mechanism to replay

--- pastisbenchmark/replayer.py
+++ pastisbenchmark/replayer.py
@@ -25,12 +25,14 @@ class Replayer(object):
     QBDI_REPLAY_DIR = "replays_qbdi"
     LLVMPROFILE_REPLAY_DIR = "replays_llvmprof"
 
-    def __init__(self, program: Path, workspace: Path, type: ReplayType, injloc: SeedInjectLoc, stream: bool = False, *args):
+    def __init__(self, program: Path, workspace: Path, type: ReplayType, injloc: SeedInjectLoc,
+                 stream: bool = False, timeout: int = 15, *args):
         self.workspace = Workspace(workspace)
         self.type = type
         self.stream = stream
         self.program = Path(program)
         self._inject_loc = injloc
+        self._timeout = timeout
         self._args = list(args)
 
         # initiatialize directories
@@ -87,7 +89,8 @@ class Replayer(object):
                              args=args,
                              output_path=str(out_file.absolute()),
                              stdin_file=input if self._inject_loc == SeedInjectLoc.STDIN else None,
-                             cwd=self.program.parent)
+                             cwd=self.program.parent,
+                             timeout=self._timeout)
 
     def _replay_llvm_profile(self, input: Path) -> bool:
         pass
