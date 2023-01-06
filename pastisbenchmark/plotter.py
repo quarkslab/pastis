7dce1f959467f5f238d198c08ce015a6a0b674ee
RobinDavid <dev.robin.david@gmail.com>
2023-01-06 17:26:08 +0100

    various updates

--- pastisbenchmark/plotter.py
+++ pastisbenchmark/plotter.py
@@ -1,9 +1,12 @@
 # built-in imports
 import logging
 from pathlib import Path
-from typing import Union, List
+from typing import Union, List, Dict
 import matplotlib.pyplot as plt
 import json
+from datetime import timedelta
+from hashlib import md5
+import base64
 
 # third-party imports
 from rich.console import Console
@@ -51,10 +54,18 @@ class Plotter(object):
                 continue
             if is_all_fuzzer and campaign.is_half_duplex and not show_union:
                 continue
-            self.add_to_plot(self.ax1, self.format_fuzzer_name(campaign, fuzzer), results, max_elapsed, is_all_fuzzer)
-            self.add_to_plot(self.ax2, self.format_fuzzer_name(campaign, fuzzer), results, max_elapsed, is_all_fuzzer)
+            if campaign.is_full_duplex and not is_all_fuzzer:  # Only print the ALL fuzzer in fullduplex
+                continue
+            name = self.format_fuzzer_name(campaign, fuzzer)
+            # fmt = self.format_plot(fuzzer, campaign.is_full_duplex)
 
-    def add_to_plot(self, plot, fuzzer: str, results: List[InputCovDelta], max_elapsed, use_global: bool, annotate_tt=False, label_tt=False):
+            marker = "--" if campaign.is_half_duplex and is_all_fuzzer else "-"
+            color = self.format_plot(fuzzer, campaign.is_full_duplex)
+
+            self.add_to_plot(self.ax1, name, results, max_elapsed, is_all_fuzzer, linestyle=marker, color=color)
+            self.add_to_plot(self.ax2, name, results, max_elapsed, is_all_fuzzer, linestyle=marker, color=color)
+
+    def add_to_plot(self, plot, fuzzer: str, results: List[InputCovDelta], max_elapsed, use_global: bool, **kwargs):
         xaxe = [x.time_elapsed for x in results]
         yaxe = [(x.overall_coverage_sum if use_global else x.fuzzer_coverage_sum) for x in results]
 
@@ -62,15 +73,16 @@ class Plotter(object):
         xaxe.append(max_elapsed)
         yaxe.append(yaxe[-1])
 
-        F = [x.fuzzer for x in results]
-        plot.plot(xaxe, yaxe, label=fuzzer, linewidth=2)
+        plot.plot(xaxe, yaxe, label=fuzzer, linewidth=2, **kwargs)
 
-    def format_fuzzer_name(self, campaign: CampaignResult, fuzzer: str) -> str:
+    def format_fuzzer_name(self, campaign: CampaignResult, fuzzer: str, short: bool=False) -> str:
         if fuzzer == CampaignResult.ALL_FUZZER:
             return campaign.slug_name
         elif fuzzer == CampaignResult.SEED_FUZZER:
             return CampaignResult.SEED_FUZZER
         elif "TT" in fuzzer:
+            if short:
+                return "TritonDSE"
             config = campaign.fuzzers_config[fuzzer]
             cov_name = {CoverageStrategy.BLOCK: "B", CoverageStrategy.EDGE: "E", CoverageStrategy.PREFIXED_EDGE: "PE",
                         CoverageStrategy.PATH: "P"}[config.coverage_strategy]
@@ -88,13 +100,26 @@ class Plotter(object):
         else:
             return fuzzer
 
+    def format_plot(self, fuzzer, full_duplex) -> str:
+        if fuzzer == CampaignResult.ALL_FUZZER:
+            return "#30a230" if full_duplex else "#1f77b4"
+        elif "TT" in fuzzer:
+            return "#d62728"
+        elif "AFLPP" in fuzzer:
+            return "#ff8214"
+        elif "HF" in fuzzer:
+            return "#9467bd"
+        else:
+            return ""
+
+
     def add_triton_input(self, campaign: CampaignResult):
         if campaign.is_full_duplex:
             results = campaign.fuzzers_items[campaign.ALL_FUZZER]
-            X = [x.time_elapsed for x in results if "TT" in x.input_name]
-            Y = [x.total_coverage for x in results if "TT" in x.input_name]
-            self.ax1.plot(X, Y, 'bo', label="TT input")
-            self.ax2.plot(X, Y, 'bo', label="TT input")
+            X = [x.time_elapsed for x in results if "TT" in x.fuzzer]
+            Y = [x.overall_coverage_sum for x in results if "TT" in x.fuzzer]
+            self.ax1.plot(X, Y, 'b.', label="TT input")
+            self.ax2.plot(X, Y, 'b.', label="TT input")
         else:
             logging.warning(f"campaign:{campaign.workspace.root} not full duplex do not show triton inputs")
 
@@ -117,6 +142,27 @@ class Plotter(object):
                              seed_sharing_stats=seed_sharing_stats, smt_stats=smt_stats)
 
 
+    def _calcul_triton_input_to_broker(self, campaign: CampaignResult) -> Dict[str, str]:
+        mapping = {}
+
+        def iter_input_dir(conf, dirname):
+            tt_workspace = campaign.workspace.root / conf.workspace
+            for file in (tt_workspace / dirname).iterdir():
+                try:
+                     raw = base64.b64decode(json.loads(file.read_text())['files']['input_file'])
+                except:
+                    raw = file.read_bytes()
+                mapping[md5(raw).hexdigest()] = str(file)
+
+        for fuzzer, items in campaign.results:
+            if campaign.is_triton(fuzzer):
+                conf = campaign.fuzzers_config[fuzzer]
+                iter_input_dir(conf, "corpus")
+                iter_input_dir(conf, "worklist")
+                iter_input_dir(conf, "crashes")
+
+        return mapping
+
     def _calcul_input_stats(self, campaign: CampaignResult) -> List[InputEntry]:
         entries = []
 
@@ -127,7 +173,8 @@ class Plotter(object):
             syms = {"CC": 0, "SR": 0, "SW": 0, "DYN": 0}
             if campaign.is_triton(fuzzer):
                 conf = campaign.fuzzers_config[fuzzer]
-                for file in (Path(conf.workspace) / "corpus").iterdir():
+                tt_workspace = campaign.workspace.root / conf.workspace
+                for file in (tt_workspace / "corpus").iterdir():
                     for s in syms:
                         if s in str(file):
                             syms[s] += 1
@@ -232,81 +279,46 @@ class Plotter(object):
             console.print(table)
 
 
-    # def print_input_number_stats(self, campaign: CampaignResult):
-    #     tot_input = sum(len(y) for x, y in campaign.results)
-    #
-    #     stats = {}  # Fuzzer -> count
-    #
-    #     for fuzzer, results in campaign.results:
-    #         for item in results:
-    #             if fuzzer == campaign.ALL_FUZZER:
-    #                 date, elapsed, fuzzer_id, hash = campaign.parse_filename(item.input_name)
-    #                 if fuzzer_id in stats:
-    #                     stats[fuzzer_id] += 1
-    #                 else:
-    #                     stats[fuzzer_id] = 1
-    #             else:
-    #                 if fuzzer in stats:
-    #                     stats[fuzzer] += 1
-    #                 else:
-    #                     stats[fuzzer] = 1
-    #     print("Input number stats:")
-    #     for fuzzer, n in stats.items():
-    #         print(f"- {self.format_fuzzer_name(campaign, fuzzer)}: {n}  [{n / tot_input:.2%}]")
-    #     print(f"Total: {tot_input}")
-    #
-    #
-    # def print_coverage_stats(self, campaign: CampaignResult):
-    #     tot_input = sum(len(z.unique_coverage) for x, y in campaign.results for z in y)
-    #
-    #     stats = {}  # Fuzzer -> count
-    #
-    #     for fuzzer, results in campaign.results:
-    #         for item in results:
-    #             if fuzzer == campaign.ALL_FUZZER:
-    #                 date, elapsed, fuzzer_id, hash = campaign.parse_filename(item.input_name)
-    #                 if fuzzer_id in stats:
-    #                     stats[fuzzer_id] += len(item.unique_coverage)
-    #                 else:
-    #                     stats[fuzzer_id] = len(item.unique_coverage)
-    #             else:
-    #                 if fuzzer in stats:
-    #                     stats[fuzzer] += len(item.unique_coverage)
-    #                 else:
-    #                     stats[fuzzer] = len(item.unique_coverage)
-    #     print("Coverage stats:")
-    #     for fuzzer, n in stats.items():
-    #         print(f"- {self.format_fuzzer_name(campaign, fuzzer)}: {n}  [{n / tot_input:.2%}]")
-    #     print(f"Total: {tot_input}")
-    #
-    #
-    # def print_triton_stats(self, campaign: CampaignResult) -> None:
-    #     def tt(secs):
-    #         return str(datetime.timedelta(seconds=int(secs)))
-    #
-    #     for fuzzer, config in campaign.fuzzers_config.items():
-    #         try:
-    #             if campaign.is_triton(fuzzer):
-    #                 workdir = (campaign.workspace.root / "clients_ws") / Path(config.workspace).name
-    #                 pstats = json.loads((workdir / "metadata/pastidse-stats.json").read_text())
-    #                 sstats = json.loads((workdir / "metadata/solving_stats.json").read_text())
-    #                 print(f"---- {fuzzer}: {self.format_fuzzer_name(campaign, fuzzer)} ----")
-    #
-    #                 # Timing stats
-    #                 tot, replay_time = pstats["total_time"], pstats["replay_time"]
-    #                 sovt = sstats['total_solving_time']
-    #                 dse = tot - replay_time - sovt
-    #                 print(f"Total: {tt(tot)} | DSE: {tt(dse)} ({dse/tot:.2%}) | SMT: {tt(sovt)} ({sovt/tot:.2%}) | REPLAY: {tt(replay_time)} ({replay_time/tot:.2%})")
-    #
-    #                 # Input stats
-    #                 tots, accs, rejs = pstats["seed_received"], pstats["seed_accepted"], pstats["seed_rejected"]
-    #                 print(f"Total: {tots} | Accepted: {accs} ({accs/tots:.2%}) |  Rejected: {rejs} ({rejs/tots:.2%})")
-    #
-    #                 # Solving stats
-    #                 stot, sat, unsat, to = sstats["total_solving_attempt"], sstats["SAT"], sstats["UNSAT"], sstats["TIMEOUT"]
-    #                 print(f"Total: {stot}  SAT: {sat} ({sat/stot:.2%}) | UNSAT: {unsat} ({unsat/stot:.2%}) | TIMEOUT: {to} ({to/stot:.2%})")
-    #
-    #                 coved, uncoved = len(sstats["branch_reverted"]), len(sstats["branch_not_solved"])
-    #                 print(f"Branch resolved: {coved} | Branch not solved: {uncoved}")
-    #         except FileNotFoundError:
-    #             logging.error(f"can't find Triton stats for {fuzzer}")
+
+    def show_delta_history(self, campaign: CampaignResult) -> None:
+        console = Console()
+        def tt(secs):
+            return str(timedelta(seconds=int(secs)))
+        def pp_edge(e):
+            return f"({e[0]:#08x}-{e[1]:#08x})"
+
+        mapping = self._calcul_triton_input_to_broker(campaign)
+
+        def caract_input(name) -> str:
+            types = ["_CC_", "_SR_", "_SW_", "_DYN_"]
+            for t in types:
+                if t in name:
+                    return t[1:-1]
+            return "N/C"
+
+        table = Table(show_header=True, title="Delta History", header_style="bold magenta")
+        for col in ["Elapsed", "Fuzzer", "Type", "New", "Tot Cov", "Items"]:
+            table.add_column(col)
+        # FIXME: Ajouter une colonne pour le count
+
+        for delta in campaign.delta_items:
+
+            # Resolve broker input to triton ones (to what it was generated from)
+            typ = "-"
+            if campaign.is_triton(delta.fuzzer):
+                meta = campaign.parse_filename(delta.input_name)
+                if meta:
+                    hash = meta[3]
+                    triton_input_name = mapping.get(hash)
+                    if triton_input_name:
+                        typ = caract_input(triton_input_name)
+
+            table.add_row(tt(delta.time_elapsed),
+                          self.format_fuzzer_name(campaign, delta.fuzzer, short=True),
+                          typ,
+                          str(len(delta.overall_new_items_covered)),
+                          str(delta.overall_coverage_sum),
+                          " ".join(pp_edge(e) for e in list(delta.overall_new_items_covered)[:4])
+            )
+
+        console.print(table)
