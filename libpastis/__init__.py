ed7bb5e410c2ca9bc739d561c4521cfd5cbe9601
RobinDavid <dev.robin.david@gmail.com>
2023-03-09 18:12:58 +0100

    add simple SAST interface

--- libpastis/__init__.py
+++ libpastis/__init__.py
@@ -1,5 +1,6 @@
 from .agent import FileAgent, BrokerAgent, ClientAgent
 from .enginedesc import EngineConfiguration, FuzzingEngineDescriptor
 from .package import BinaryPackage
+from .sast import SASTAlert, SASTReport
 
 __version__ = "0.2.1"
