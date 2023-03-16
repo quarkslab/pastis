077e80c4a880934472b7dc058e1f62774a32d350
RobinDavid <dev.robin.david@gmail.com>
2023-03-16 11:06:49 +0100

    move runner in modules

--- engines/pastis-triton/pastisdse/__init__.py
+++ engines/pastis-triton/pastisdse/__init__.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+import subprocess
+
 from .pastisdse import PastisDSE
 
 # Expose triton version
@@ -6,3 +9,10 @@ import tritondse
 __version__ = "0.3"
 
 TRITON_VERSION = tritondse.TRITON_VERSION
+
+
+def spawn_online_triton(port: int = 5555, probe: Tuple[str] = ()):
+    tt = ["pastis-triton", "online", "-p", f"{port}"]
+    if len(probe) > 0:
+        tt += ["--probe", f"{probe}"]
+    subprocess.Popen(tt, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
