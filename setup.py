d790def9f50eca998529155bb2f6d729b27e6048
Christian Heitman <cheitman@quarkslab.com>
2023-07-20 10:42:47 -0300

    Bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md") as f:
 
 setup(
     name="pastis-framework",
-    version="1.0.3",
+    version="1.0.4",
     description="PASTIS framework for collaborative fuzzing",
     long_description=README,
     long_description_content_type='text/markdown',
